35799f667d53b8b21c0fba8af9fa6fc9d89b0a37
MAT�AS JOAQUIN CASTERA <48385887@ort>
2025-03-21 08:57:21 -0300

    asd

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
-function validarLongitud()
+const validarNLongitud = () => 
 {
     let nombre= document.getElementById("nombre").value;
     let mensajeError = document.getElementById("mensajeError"); 
    
-    if(nombre.length >= 3 || nombre.length==0)
+    if(nombre.length >= 3)
     {
         
-        mensajeError.innerHTML = "";
-        return true;
+        mensajeError.innerHTML = "Es Valido";
+        mensajeError.style.color = "Green"
     }
     else{
         
         mensajeError.innerHTML = "El nombre completo debe tener al menos 3 caracteres";
-        return false;
+        mensajeError.style.color = "Red"
     }
 }
 
-function MostrarMensaje()
-{
-    let ConfirmarPassword= document.getElementById("ConfirmarPassword").value;
-    let Password= document.getElementById("password").value;
-    let error= document.getElementById("errorPassword").value;
 
-    if(ConfirmarPassword !== password)
+const MostrarMensaje = () =>
+{
+    let Password = document.getElementById("password").value;
+    let ConfirmarPassword = document.getElementById("ConfirmarPassword").value;
+    let errorPassword = document.getElementById("errorPassword");
+    let errorConfirmarPassword = document.getElementById("errorConfirmarPassword");
+
+    if(Password.length >= 8 && (/[1234567890]/.test(Password)) && (/[a-z]/.test(Password))) 
     {
-        error.innerHTML="Las constraseñas no coinciden"
-        return false
+        errorPassword.innerHTML = "Es valida la contraseña"
+        esValido = true
     }
+    else
+    {
+        errorPassword.innerHTML = "Debe contener 8 caracteres, una letra y un numero"
+        esValido = false
+
+    }
+
+    if(ConfirmarPassword != Password)
+    {
+        errorConfirmarPassword.innerHTML = "Las constraseñas no coinciden"
+        esValido = false
+
+    }
+    else 
+    {
+        errorConfirmarPassword.innerHTML = "Es valido"
+        esValido = true
+    }
+
+    return esValido
+}
+
+const Regirtrarse = () => {
+    return esValido
+
 }
